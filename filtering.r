4730f20c2764e033262a3ffd37efc04a606b7a5e
Pranav Gundrala <pgundral@pranavsmbp2716.mynetworksettings.com>
2025-02-25 02:56:48 -0500

    cleaned, yellowstone occuring

--- filtering.r
+++ filtering.r
@@ -18,29 +18,36 @@ barcode_sums <- bold %>%
 
 # find appropriate families by sorting sums and filtering to a given sum
 nb_families <- arrange(barcode_sums, by = barcode_sums$row_sum) %>% select(ITIS.Family.Name, row_sum)
-nb_families <- nb_families %>% filter(row_sum <= 50) # returns between 0 and 100
+nb_families <- nb_families %>% filter(row_sum <= 100) # returns between 0 and 100
 
-# find all angiosperm families from ITIS 'Magnoliopsida'
-angio <- downstream('Magnoliopsida', db = 'itis', downto = 'family')
-angio_families <- angio$Magnoliopsida$taxonname
+# find all trachoephyte families from ITIS 'Tracheophyta'
+tracheo <- downstream('Tracheophyta', db = 'itis', downto = 'family')
+tracheo_families <- tracheo$Tracheophyta$taxonname
+
+# # find all angiosperm families from ITIS 'Magnoliopsida'
+# angio <- downstream('Magnoliopsida', db = 'itis', downto = 'family')
+# angio_families <- angio$Magnoliopsida$taxonname
 
 # subset all no-barcode families to only angiosperm families
 families <- nb_families$ITIS.Family.Name[nb_families$ITIS.Family.Name %in% 
-                        angio_families]
+                        tracheo_families]
 
+# write families to csv
 write.csv(families, file='./data/families.csv', row.names=FALSE, 
-quote=FALSE) # write to csv
+quote=FALSE)
 
 # taxize::get_tsn() to get ITIS Taxonomic Serial No.s
 tsn <- get_tsn(families)
-write.csv(tsn, file='./data/tsn.csv', row.names=FALSE, quote=FALSE) # write to csv
+# write tsns to csv
+write.csv(tsn, file='./data/tsn.csv', row.names=FALSE, quote=FALSE)
 
 # find all downstream genera using taxize::downstream()
 genera <- downstream(tsn, db = 'itis', downto = 'genus')
 genera_list <- do.call("rbind", genera)$taxonname 
 genera_list <- genera_list[genera_list != "No data"]
+# write genera to csv
 write.csv(genera_list, file='./data/genera.csv', row.names=FALSE,
-quote=FALSE) # write to csv
+quote=FALSE)
 
 # collapse into searchable term
-write(paste(genera_list, collapse = ', '), 'search_50.txt')
+write(paste(genera_list, collapse = ', '), 'searchable-100.txt')
--- search.r
+++ search.r
@@ -1,3 +1,33 @@
+library(readxl)
+library(tidyverse)
+
+# Read in BRU data
 occurrences <- read.csv("./data/BrownUniversity-BRU_DwC-A/occurrences.csv")
 
-head(occurrences)
+# Read in genera identified by ITIS processor 
+genera <- read.csv("data/genera.csv")
+genera <- data.frame(genera)
+
+colnames(genera) <- 'genus'
+
+# Find only specimens matching the genera
+match <- inner_join(occurrences, genera, by = "genus")
+
+# Identify only specimens from the USA from 1975-present
+specimens <- match %>% filter(year >= 1975, country == 'United States of America')
+
+# Read in Yellowstone occuring species data from NPSpecies
+yellowstone <- read_excel("data/NPSpecies_Checklist_YELL_20250223195402.xlsx")
+
+# Update all species names to exclude var.
+yellowstone$`Scientific Name` <- sub(" var\\..*", "", yellowstone$`Scientific Name`)
+
+# Narrow identified BRU specimens that occur in yellowstone
+yellowstone_specimens <- specimens[which(specimens$scientificName %in% yellowstone$`Scientific Name`),]
+
+# write YELL specimen IDs to CSV
+write(paste(yellowstone_specimens$occurrenceID, collapse = ', '), 'yellowstone_specimen_ids.txt')
+
+# write other specimen IDs to CSV
+temp <- specimens$occurrenceID[!(specimens$occurrenceID %in% yellowstone_specimens$occurrenceID)]
+write(paste(temp, collapse = ', '), 'other_specimen_ids.txt')
