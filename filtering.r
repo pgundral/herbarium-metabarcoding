a4dcefad69b53353ee2e40f77692f1185790e4b0
Pranav Gundrala <pgundral@Pranavs-MacBook-Pro-2716.local>
2025-03-03 16:53:57 -0500

    sorting by total barcodes and trnL seq

--- filtering.r
+++ filtering.r
@@ -12,42 +12,58 @@ bold <- bold %>% mutate(across(everything(), ~ replace(., is.na(.), 0)))
 # find no-barcode families by summing over seq regions
 barcode_sums <- bold %>%
   mutate(row_sum = rowSums(across(-c(ITIS.Family.Name, ITIS.Species.Count, BOLD.Specimen.Count, 
-                                     BOLD.Species.Count)))) 
+                                     BOLD.Species.Count)))) %>%
+  mutate(trnL_count = BOLD.trnL.Specimen.Count)
 
-# head(barcode_sums)
+head(barcode_sums)
 
 # find appropriate families by sorting sums and filtering to a given sum
-nb_families <- arrange(barcode_sums, by = barcode_sums$row_sum) %>% select(ITIS.Family.Name, row_sum)
-nb_families <- nb_families %>% filter(row_sum <= 100) # returns between 0 and 100
+nb_families <- arrange(barcode_sums, by = barcode_sums$row_sum) %>% select(ITIS.Family.Name, row_sum, trnL_count)
+nb_families <- nb_families %>% filter(row_sum <= 100) # returns low-barcode families
 
 # find all trachoephyte families from ITIS 'Tracheophyta'
 tracheo <- downstream('Tracheophyta', db = 'itis', downto = 'family')
 tracheo_families <- tracheo$Tracheophyta$taxonname
 
+# write.csv(tracheo_families, file='./data/trach_families_itis.csv')
+# tracheo_families = read.csv("./data/trach_families_itis.csv")
+
 # # find all angiosperm families from ITIS 'Magnoliopsida'
 # angio <- downstream('Magnoliopsida', db = 'itis', downto = 'family')
 # angio_families <- angio$Magnoliopsida$taxonname
 
-# subset all no-barcode families to only angiosperm families
-families <- nb_families$ITIS.Family.Name[nb_families$ITIS.Family.Name %in% 
-                        tracheo_families]
+# subset all no-barcode families to only tracheophyte families
+# families <- nb_families[nb_families$ITIS.Family.Name %in% 
+#                         tracheo_families,]
+
+families <- nb_families %>% filter(ITIS.Family.Name %in% tracheo_families)
+
+families <- families %>% arrange(row_sum, trnL_count)
 
 # write families to csv
 write.csv(families, file='./data/families.csv', row.names=FALSE, 
 quote=FALSE)
 
 # taxize::get_tsn() to get ITIS Taxonomic Serial No.s
-tsn <- get_tsn(families)
+tsn <- get_tsn(families$ITIS.Family.Name)
 # write tsns to csv
 write.csv(tsn, file='./data/tsn.csv', row.names=FALSE, quote=FALSE)
 
 # find all downstream genera using taxize::downstream()
 genera <- downstream(tsn, db = 'itis', downto = 'genus')
-genera_list <- do.call("rbind", genera)$taxonname 
-genera_list <- genera_list[genera_list != "No data"]
+
+genera_list <- do.call("rbind", genera) %>% select(parentname, taxonname)
+genera_list <- genera_list %>% filter(taxonname != "No data")
+
+colnames(families) <- c("parentname", "total_barcodes", "trnL_barcodes")
+genera_list <- inner_join(families, genera_list, by = "parentname") %>% 
+               select(parentname, taxonname, total_barcodes, trnL_barcodes)
+
+genera_list <- genera_list %>% arrange(total_barcodes, trnL_barcodes)
+
 # write genera to csv
 write.csv(genera_list, file='./data/genera.csv', row.names=FALSE,
 quote=FALSE)
 
 # collapse into searchable term
-write(paste(genera_list, collapse = ', '), 'searchable-100.txt')
+write(paste(genera_list$taxonname, collapse = ', '), 'searchable.txt')
--- search.r
+++ search.r
@@ -8,13 +8,21 @@ occurrences <- read.csv("./data/BrownUniversity-BRU_DwC-A/occurrences.csv")
 genera <- read.csv("data/genera.csv")
 genera <- data.frame(genera)
 
-colnames(genera) <- 'genus'
+colnames(genera) <- c('ITIS_Family','genus','total_barcodes', 'trnL_barcodes')
 
 # Find only specimens matching the genera
-match <- inner_join(occurrences, genera, by = "genus")
+match <- inner_join(occurrences, genera, by = "genus") %>% mutate(BRU_Family = family) %>%
+select(total_barcodes, trnL_barcodes, ITIS_Family, BRU_Family, genus, scientificName, year, 
+country, stateProvince, occurrenceID)
+
+# SPECIMENS FOR SAMPLING NO YEAR RESTRICTION ONLY 0 BARCODE #
+specimens <- match %>% arrange(total_barcodes, trnL_barcodes, ITIS_Family, genus, desc(year))
+sliced <- specimens %>% group_by(ITIS_Family) %>% slice(1) %>% ungroup() %>% arrange(total_barcodes, trnL_barcodes)
+write.csv(specimens, file='./data/specimens.csv')
+write.csv(sliced_specimens, file='./data/sliced_specimens.csv')
 
 # Identify only specimens from the USA from 1975-present
-specimens <- match %>% filter(year >= 1975, country == 'United States of America')
+#specimens <- match %>% filter(year >= 1975, country == 'United States of America')
 
 # Read in Yellowstone occuring species data from NPSpecies
 yellowstone <- read_excel("data/NPSpecies_Checklist_YELL_20250223195402.xlsx")
@@ -26,6 +34,7 @@ yellowstone$`Scientific Name` <- sub(" var\\..*", "", yellowstone$`Scientific Na
 yellowstone_specimens <- specimens[which(specimens$scientificName %in% yellowstone$`Scientific Name`),]
 
 # write YELL specimen IDs to CSV
+write.csv(yellowstone_specimens, file='./data/yellowstone_specimens.csv')
 write(paste(yellowstone_specimens$occurrenceID, collapse = ', '), 'yellowstone_specimen_ids.txt')
 
 # write other specimen IDs to CSV
